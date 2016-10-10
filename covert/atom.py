290bf62a47b15f46a27b7a0ddbb50490a2bfb285
Nico Poppelier <n.poppelier@xs4all.nl>
2016-10-10 16:19:04 +0200

    added new concept: query maps

--- covert/atom.py
+++ covert/atom.py
@@ -11,13 +11,14 @@ from datetime import datetime, date, time, MINYEAR
 from .common import Error
 
 class Atom:
-    __slots__ = ('schema', 'convert', 'display', 'formtype', 'control',
+    __slots__ = ('schema', 'convert', 'display', 'query', 'formtype', 'control',
                  'default', 'read', 'write', 'enum')
-    def __init__(self, schema, convert, display, formtype, control,
+    def __init__(self, schema, convert, display, query, formtype, control,
                  default='', read=None, write=None, enum=None):
         self.schema   = schema
         self.convert  = convert
         self.display  = display
+        self.query    = query
         self.formtype = formtype
         self.control  = control
         self.default  = default
@@ -40,12 +41,23 @@ MIDNIGHT       = time(0, 0, 0, 0)
 true_strings = ('j', 'ja') # TODO: I18N
 # EN: yes/no or y/n, SV: ja/nej
 bool_repr = {True:'ja', False:'nee'}   # TODO: I18N
-# identity = lambda x: x # when we use dense/full transformation maps
-identity = None # when we use sparse transformation maps (faster)
 
+# Tree transformations are a cornerstone of the framework. There are two types
+# of transformation maps: (1) dense or full maps, where every node has a mapping function,
+# possibly the identity, or (2) spare maps, where a node has a mapping function only
+# when necessary (other nodes get mapping function=None). Performance measurements
+# show that option 2 is faster.
+identity = None # since we use sparse transformation maps
+
+# Query transformations use 2-tuples and 3-tuples. The first member of the tuple is
+# a 2-character operator: == for equal, =~ for regex match (Perl5 influence in a Python application),
+# [] for 'between boundaries (boundaries included)' etcetera.
+
+boolean_convert = lambda value: value.lower() in true_strings,
 register_atom('boolean',
     schema   = bool,
-    convert  = lambda value: value.lower() in true_strings,
+    convert  = boolean_convert,
+    query    = lambda value: ('==', boolean_convert(value)),
     display  = lambda value: bool_repr[value],
     formtype = 'boolean',
     enum     = ['nee', 'ja'],
@@ -53,10 +65,12 @@ register_atom('boolean',
 )
 
 # date and datetime use str.format in the display maps, to avoid Python issue 13305
+date_convert = lambda value: datetime.strptime(value, "%Y-%m-%d")
 register_atom('date',
     schema   = date,
-    convert  = lambda value: datetime.strptime(value, "%Y-%m-%d"),
+    convert  = date_convert,
     display  = lambda value: '{0:04d}-{1:02d}-{2:02d}'.format(value.year, value.month, value.day),
+    query    = lambda value: ('==', date_convert(value)),
     read     = lambda value: value.date(),
     write    = lambda value: datetime.combine(value, MIDNIGHT),
     default  = EMPTY_DATE,
@@ -64,11 +78,13 @@ register_atom('date',
     control  = 'input'
 )
 
+datetime_convert =lambda value: datetime.strptime(value, "%Y-%m-%dT%H:%M:%S")
 register_atom('datetime',
     schema   = datetime,
-    convert  = lambda value: datetime.strptime(value, "%Y-%m-%dT%H:%M:%S"),
+    convert  = datetime_convert,
     display  = lambda value: '{0:04d}-{1:02d}-{2:02d}T{3:02d}:{4:02d}:{5:02d}'.\
                format(value.year, value.month, value.day, value.hour, value.minute, value.second),
+    query    = lambda value: ('==', datetime_convert(value)),
     default  = EMPTY_DATETIME,
     formtype = 'datetime',
     control  = 'input'
@@ -78,6 +94,7 @@ register_atom('float',
     schema   = float,
     convert  = float,
     display  = lambda value: '{0:.2}'.format(value),
+    query    = lambda value: ('==', float(value)),
     formtype = 'number',
     control  = 'input'
 )
@@ -87,6 +104,7 @@ register_atom('gender', # TODO: add possibility to define this in application
     schema   = int,
     convert  = int,
     display  = lambda value: genders[value],
+    query    = lambda value: ('==', int(value)),
     formtype = 'gender',
     control  = 'radio',
     default  = 0,
@@ -97,6 +115,7 @@ register_atom('integer',
     schema   = int,
     convert  = int,
     display  = str,
+    query    = lambda value: ('==', int(value)),
     formtype = 'number',
     control  = 'input'
 )
@@ -105,6 +124,7 @@ register_atom('memo',
     schema   = str,
     convert  = identity,
     display  = identity,
+    query    = lambda value: ('=~', value),
     formtype = 'memo',
     control  = 'textarea'
 )
@@ -113,6 +133,7 @@ register_atom('string',
     schema   = str,
     convert  = identity,
     display  = identity,
+    query    = lambda value: ('=~', value),
     formtype = 'text',
     control  = 'input'
 )
@@ -121,14 +142,17 @@ register_atom('text',
     schema   = str,
     convert  = identity,
     display  = identity,
+    query    = lambda value: ('=~', value),
     formtype = 'text',
     control  = 'textarea'
 )
 
+time_convert = lambda value: datetime.strptime(value, "%H:%M:%S")
 register_atom('time',
     schema   = time,
-    convert  = lambda value: datetime.strptime(value, "%H:%M:%S"),
+    convert  = time_convert,
     display  = lambda value: '{0:02d}:{1:02d}:{2:02d}'.format(value.hour, value.minute, value.second),
+    query    = lambda value: ('==', time_convert(value)),
     default  = EMPTY_TIME,
     formtype = 'datetime',
     control  = 'input'
@@ -138,6 +162,7 @@ register_atom('url',
     schema   = str,
     convert  = identity,
     display  = identity,
+    query    = lambda value: ('=~', value),
     formtype = 'url',
     control  = 'input'
 )
