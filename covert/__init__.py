6e479db6b0db0766823c4aa3afb9083ca54972ae
Nico Poppelier <n.poppelier@xs4all.nl>
2016-10-23 14:44:59 +0200

    improved comments

--- covert/__init__.py
+++ covert/__init__.py
@@ -5,23 +5,23 @@ Copyright: ...
 License: ...
 
 Attributes:
-    setting:       global settings
+    * setting:       module object with global settings
 
 Functions:
-    read_config:   read configuration file
-    parse_cmdline: parse command line
-    kernel_init:   initialize kernel
-    http_server:   development server based on Waitress
+    * read_config:   read configuration file
+    * parse_cmdline: parse command line
+    * kernel_init:   initialize kernel
+    * http_server:   development server based on Waitress
 
 Classes:
-    SwitchRouter:  WSGI front-end application that dispatches to WSGI applications for
-        static files, HTML fragment, HTML page and JSON
-    MapRouter:     WSGI application that dispatches on the basis of PATH_INFO
-    PageRouter:    sub-class of MapRouter for HTML pages
-    JSONRouter:    sub-class of MapRouter for JSON
-    route:         decorator for methods in an ItemView class
-    BareItemView:  view that does not define routes
-    ItemView:      view with routes for the Atom Publishing protocol
+    * SwitchRouter:  WSGI front-end application that dispatches to WSGI applications for
+    *                static files, HTML fragment, HTML page and JSON
+    * MapRouter:     WSGI application that dispatches on the basis of PATH_INFO
+    * PageRouter:    sub-class of MapRouter for HTML pages
+    * JSONRouter:    sub-class of MapRouter for JSON
+    * route:         decorator for methods in an ItemView class
+    * BareItemView:  view that does not define routes
+    * ItemView:      view with routes for the Atom Publishing protocol
 """
 
 from .           import setting
