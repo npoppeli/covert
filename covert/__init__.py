ed5100d0c7c0b012aba0ecb6fd2cb3a92d06fff0
Nico Poppelier <n.poppelier@xs4all.nl>
2016-12-11 19:55:55 +0100

    removed unnecessary exports

--- covert/__init__.py
+++ covert/__init__.py
@@ -6,7 +6,6 @@ License: ...
 
 Attributes:
     * setting:        module object with global settings
-    * patterns:       abbreviations for regular expressions in routes
 
 Functions:
     * read_config:    read configuration file
@@ -16,7 +15,6 @@ Functions:
     * http_server:    development server based on Waitress
     * url_for:        create URL for route
     * show_dict:      pretty-print dictionary
-    * split_route:    split route into components
     * route2regex:    create regular expression from route specifier
     * route2pattern:  create formatting string from route specifier
 
@@ -39,4 +37,4 @@ from .config     import read_config, parse_cmdline, kernel_init
 from .controller import http_server, SwitchRouter, MapRouter, PageRouter, JSONRouter
 from .model      import ItemRef
 from .view       import route, BareItemView, ItemView, url_for, show_dict, encode_dict
-from .view       import patterns, split_route, route2pattern, route2regex
+from .view       import route2pattern, route2regex
