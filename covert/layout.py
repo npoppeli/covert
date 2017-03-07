473873aff0fdd8dec143f472a588c7541bf87f9d
Nico Poppelier <n.poppelier@xs4all.nl>
2017-03-07 21:42:39 +0100

    improved support for other template engines

--- covert/layout.py
+++ covert/layout.py
@@ -9,53 +9,64 @@ import sys
 from os import walk
 from os.path import join, splitext, relpath
 from . import setting
-from .common import logger, read_file
+from .common import logger
+from .controller import exception_report
 
 # By default, there is one template factory: chameleon.PageTemplateFile. This is associated
-# with the file extension '.pt' (suffix 'pt'). Other template
-# factories can be defined, provided they implement the following interface:
+# with the file extension '.pt'. Other template factories can be defined, provided they
+# implement the following interface:
 #   - filename -> template: template = template_factory(filename)
-#   - node     -> string: template.render(this=node) returns HTML page for node 'this'
-#                 (condition: node.skin should be equal to filename without extension)
-# Template factories are stored in a dictionary template_factory, with the suffix as key.
+#   - context  -> string: template.render(this=context) returns HTML page for context 'this' (dict)
+# Template factories are stored in a dictionary template_factory, with the extension as key.
 
-# TODO: Tonnikala is added here temporarily, but should move to application level
+# TODO: Tonnikala and Mako are added here temporarily, but should move to application level
 template_factory = {}
 
-def add_template_type(suffix, factory):
+def add_template_type(extension, factory):
     """Add template factory for given suffix.
 
-    :param suffix: file extension minus the period
-    :param factory: template factory (callable object)
+    :param extension: file extension (.foo)
+    :param factory:   template factory (callable object)
 
     Returns:
         None
     """
-    if suffix in template_factory:
-        logger.debug('Cannot redefine template type %s', suffix)
+    if extension in template_factory:
+        logger.debug('Cannot redefine template type %s', extension)
     else:
-        logger.debug('Define new template type %s', suffix)
-        template_factory[suffix] = factory
+        logger.debug('Define new template type %s', extension)
+        template_factory[extension] = factory
 
 try:
     from chameleon import PageTemplateFile
-    add_template_type('pt', PageTemplateFile)
-except ImportError:
-    logger.critical('Chameleon template engine not available')
+    add_template_type('.pt', PageTemplateFile)
+except ImportError as e:
+    logger.critical('Chameleon template engine not available (%s)', e)
     sys.exit(1)
 
 try:
-    from tonnikala import Loader
-    class TkTemplateFile(Loader):
+    from tonnikala import FileLoader
+    tkLoader = FileLoader()
+    class TkTemplateFile:
         def __init__(self, path):
             super().__init__()
-            self.template = self.load_string(read_file(path))
-            self.path = path
+            if not tkLoader.paths:
+                tkLoader.add_path(setting.layout)
+            self.template = tkLoader.load(relpath(path, setting.layout))
         def render(self, **vars):
             return self.template.render_to_buffer(vars, '__main__').join()
-    add_template_type('tk', TkTemplateFile)
-except ImportError:
-    logger.critical('Tonnikala template engine not available')
+    add_template_type('.tk', TkTemplateFile)
+except ImportError as e:
+    logger.critical('Tonnikala template engine not available (%s)', e)
+    sys.exit(1)
+
+try:
+    from mako.template import Template
+    def makoTemplateFile(path):
+        return Template(filename=path)
+    add_template_type('.mko', makoTemplateFile)
+except ImportError as e:
+    logger.critical('Mako template engine not available (%s)', e)
     sys.exit(1)
 
 def read_templates():
@@ -73,15 +84,15 @@ def read_templates():
         prefix = relpath(dirpath, setting.layout)
         for filename in filenames:
             name, extension = splitext(filename)
-            suffix = extension[1:]
-            if suffix in template_types:
+            if extension in template_types:
                 if prefix == '.':
                     template_name = name
                 else:
                     template_name = prefix.replace('/', '_')+'_'+name
                 file_path = join(dirpath, filename)
                 try:
-                    setting.templates[template_name] = template_factory[suffix](file_path)
+                    setting.templates[template_name] = template_factory[extension](file_path)
+                    # logger.debug("Template {} is in file {}".format(template_name, relpath(file_path, setting.layout)))
                 except Exception as e:
                     logger.error("Error in template '{0}' in file {1}".format(template_name, file_path))
-                    logger.error(str(e))
+                    logger.error(exception_report(e, ashtml=False))
