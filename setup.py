7a8713bb4fa144086f2083c665a147a03c2c316f
Nico Poppelier <n.poppelier@xs4all.nl>
2018-04-30 14:45:59 +0200

    incremented version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='covert',
-    version='0.8',
+    version='0.9',
     author='Nico Poppelier',
     author_email='n.poppelier@xs4all.nl',
     license='MIT',
