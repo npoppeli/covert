389e769d273686ba20d64bbd6f95caa170de6089
Nico Poppelier <n.poppelier@xs4all.nl>
2019-07-06 11:48:55 +0200

    added event handling

--- covert/__init__.py
+++ covert/__init__.py
@@ -8,29 +8,29 @@ Attributes:
     * setting:        module object with global settings
 
 Functions:
-    * read_configuration: read configuration file
-    * parse_cmdline:      parse command line
-    * add_argument:       add argument to argument parser
-    * read_yaml_file:     read YAML file, return one or more documents
-    * initialize_kernel:  initialize kernel
-    * http_server:        development server based on Waitress
-    * url_for:            create URL for route
-    * show_dict:          pretty-print dictionary
-    * route2regex:        create regular expression from route specifier
-    * route2pattern:      create formatting string from route specifier
+    * read_configuration:    read configuration file
+    * parse_cmdline:  parse command line
+    * add_argument:   add argument to argument parser
+    * read_yaml_file: read YAML file, return one or more documents
+    * initialize_kernel:    initialize kernel
+    * http_server:    development server based on Waitress
+    * url_for:        create URL for route
+    * show_dict:      pretty-print dictionary
+    * route2regex:    create regular expression from route specifier
+    * route2pattern:  create formatting string from route specifier
 
 
 Classes:
     * CondRouter:   WSGI front-end application that dispatches to WSGI applications for
-    *               static files, HTML fragment, HTML page and JSON
-    * MapRouter:    WSGI application that dispatches on the basis of PATH_INFO
-    * PageRouter:   sub-class of MapRouter for HTML pages
-    * JSONRouter:   sub-class of MapRouter for JSON
-    * route:        decorator for methods in an ItemView class
-    * BareItemView: view that does not define routes
-    * ItemView:     view with routes for the Atom Publishing protocol
-    * ItemRef:      base class for item references
-    * Visitor:      base class for Visitors
+    *                 static files, HTML fragment, HTML page and JSON
+    * MapRouter:      WSGI application that dispatches on the basis of PATH_INFO
+    * PageRouter:     sub-class of MapRouter for HTML pages
+    * JSONRouter:     sub-class of MapRouter for JSON
+    * route:          decorator for methods in an ItemView class
+    * BareItemView:   view that does not define routes
+    * ItemView:       view with routes for the Atom Publishing protocol
+    * ItemRef:        base class for item references
+    * Visitor:        base class for Visitors
 """
 
 from .           import setting
@@ -38,6 +38,7 @@ from .atom       import EMPTY_DATE, EMPTY_DATETIME, EMPTY_TIME, define_atom
 from .common     import *
 from .config     import read_configuration, parse_cmdline, initialize_kernel, add_argument
 from .controller import http_server, CondRouter, MapRouter, PageRouter, JSONRouter
+from .event      import add_handler, event
 from .layout     import add_template_type
 from .model      import ItemRef, Visitor, unflatten
 from .view       import route, BareItemView, ItemView, url_for
---
+++ covert/event.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""Objects and functions related to events.
+"""
+
+from .common import logger
+from . import common as c
+
+event_handler = {}
+
+def add_handler(evnt, handler):
+    """add handler for event"""
+    if callable(handler):
+        if evnt in event_handler:
+            logger.debug(c._('Cannot override handler for {}').format(evnt))
+        else:
+            logger.debug(c._('New event handler for {}').format(evnt))
+            event_handler[evnt] = handler
+    else:
+        logger.debug(c._('Event handler for {} is not a callable').format(evnt))
+
+def event(evnt, node):
+    """call handler for event 'event'"""
+    if evnt in event_handler:
+        event_handler[evnt](node)
